b60902c34a14f26697bc1e41d7ea573ccef66f8e
yzaripova <zaripovaya@gmail.com>
2017-12-16 18:59:44 +0500

    Add requestAnimationFrame for draw

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ let formElement;
 let canvas;
 let ctx;
 let paddles = [];
-let ball = {};
 let options;
 let gameInfo;
+let fps;
+let lastKeyDownCode;
+let isAnimating;
 
 function init() {
   ws = new WebSocket('ws://localhost:3001');
@@ -24,7 +26,7 @@ function init() {
 
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
-  document.addEventListener('keydown', updatePosition, false);
+  document.addEventListener('keydown', updatePaddlePosition, false);
 }
 
 function onUpdateData(event) {
@@ -32,12 +34,15 @@ function onUpdateData(event) {
   
   if (message.event && message.event === 'connected') {
     const lobbyData = message.lobby;
-    onLoadFirstPlayerInfo(lobbyData.users[0]);
-    onShowLobby();
 
     if (lobbyData.users.length > 1) {
-      onLoadSecondPlayerInfo(lobbyData.users[1]);
+      onLoadFirstPlayerInfo(lobbyData.users[1]);
+      onLoadSecondPlayerInfo(lobbyData.users[0]);
+    } else {
+      onLoadFirstPlayerInfo(lobbyData.users[0]);
     }
+
+    onShowLobby();
   }
 
   if (message.event && message.event === 'user-connected') {
@@ -47,8 +52,10 @@ function onUpdateData(event) {
   if (message.event && message.event === 'get-ready') {
     showTimer(message.countdown);
     document.querySelector('body').classList.add('loading');
+
     canvas.width = message.options.fieldSize.x;
     canvas.height = message.options.fieldSize.y;
+
     paddles.push(new Paddle('bottom', message.options.racketWidth, 
       message.options.racketThickness));
     paddles.push(new Paddle('top', message.options.racketWidth, 
@@ -57,19 +64,37 @@ function onUpdateData(event) {
     gameInfo = message.game;
     options = message.options;
 
-    drawPlayingField(message.options, message.game);
+    draw();
     onShowGame();
   }
 
   if (message.event && message.event === 'started') {
     document.querySelector('body').classList.remove('loading');
+    gameInfo = message.game;
+    startAnimation();
+  }
+
+  if (message.event && message.event === 'sync') {
+    if (lastKeyDownCode) {
+      onSendUserPaddle(lastKeyDownCode);
+    }
+    gameInfo = message.game;
+    draw();
+  }
+
+  if (message.event && message.event === 'user-disconnected') {
+    onShowLobby();
+    onSendMessage({action: 'not-ready'});
   }
 }
 
 function onShowLobby() {
   const lobbyElement = document.querySelector('.lobby');
   const loginElement = document.querySelector('.login');
+  const playingFieldElement = document.querySelector('.playing-field');
+
   loginElement.classList.add('hide');
+  playingFieldElement.classList.add('hide');
   lobbyElement.classList.remove('hide');
   lobbyElement.classList.add('show');
 }
@@ -77,26 +102,46 @@ function onShowLobby() {
 function onShowGame() {
   const lobbyElement = document.querySelector('.lobby');
   const playingFieldElement = document.querySelector('.playing-field');
+
   lobbyElement.classList.add('hide');
   lobbyElement.classList.remove('show');
   playingFieldElement.classList.remove('hide');
   playingFieldElement.classList.add('show');
 }
 
-function drawPlayingField(options, gameInfo) {
+function startAnimation() {
+  let now;
+  let then = Date.now();
+  let delay = 1000/fps;
+  let delta;
+  isAnimating = true;
+
+  requestAnimationFrame(startAnimation);
+     
+    now = Date.now();
+    delta = now - then;
+    
+    if (delta > delay && isAnimating) {
+      then = now - (delta % delay);
+        draw();
+    }
+}
+
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = '#228b22';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  drawBall({x: gameInfo.ball.x, y: gameInfo.ball.y, r: 
-    options.ballSize/2});
+  drawBall();
   drawPaddles();
   ctx.fillRect(0, canvas.height/2 - 1, canvas.width, 2);
 }
 
-function drawBall(ball) {
+function drawBall() {
+  let ball = gameInfo.ball;
+  let radius = options.ballSize / 2;
   ctx.beginPath();
   ctx.fillStyle = '#fff';
-  ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI*2, false);
+  ctx.arc(ball.x, ball.y, radius, 0, Math.PI*2, false);
   ctx.fill();
 }
 
@@ -107,21 +152,16 @@ function drawPaddles() {
   });
 }
 
-function Paddle(pos, width, height) {
-  this.h = height;
-  this.w = width;
-  
-  this.x = canvas.width/2 - this.w/2;
-  this.y = (pos === 'top') ? 0 : canvas.height - this.h;
-}
-
-function updatePosition(event) {
+function updatePaddlePosition(event) {
   if (event.keyCode !== 37 && event.keyCode !== 39) {
     return;
   }
 
+  lastKeyDownCode = event.keyCode;
+
   if (event.keyCode === 37) {
     paddles[0].x -= 10;
+
     if (paddles[0].x < 0) {
       paddles[0].x = 0;
     }
@@ -135,7 +175,7 @@ function updatePosition(event) {
     }
   }
 
-  drawPlayingField(options, gameInfo);
+  draw();
 }
 
 function showTimer(time) {
@@ -194,4 +234,21 @@ function onSendUserInfo(event) {
     action: 'join',
     user: result
   });
+}
+
+function onSendUserPaddle(keyCode) {
+  onSendMessage({
+    action: 'move',
+    x: paddles[0].x,
+    y: paddles[0].y,
+    keyCode: keyCode
+  })
+}
+
+function Paddle(pos, width, height) {
+  this.h = height;
+  this.w = width;
+  
+  this.x = canvas.width/2 - this.w/2;
+  this.y = (pos === 'top') ? 0 : canvas.height - this.h;
 }
